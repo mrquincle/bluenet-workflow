5496e8b28cb3cbb30ee8eff77d19b59e70843394
Anne van Rossum <anne@crownstone.rocks>
2021-06-11 13:41:46 +0200

    Make debug script interactive

--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 board=${1:? "usage: $0 board" }
 
 cd ../bluenet/build
@@ -5,10 +7,34 @@ cd $board
 
 echo "Run the following in three different shells"
 
-echo "make uart_client"
-echo "make debug_server"
-echo "make debug_application"
+echo "1. make uart_client"
+echo "2. make debug_server"
+echo "3. make debug_application"
+echo "4. make reset (optionally)"
+echo "5. make uart_binary_client (alternative for 1)"
 
-echo ""
-echo "Eventually:"
-echo "make reset"
+echo -n  "Make your choice [1-5]: "
+
+read cmd
+
+case $cmd in
+	1)
+		make uart_client
+		;;
+	2)
+		make debug_server
+		;;
+	3)
+		make debug_application
+		;;
+	4)
+		make reset
+		;;
+	5)
+		make uart_binary_client
+		;;
+	*)
+		echo "Unknown option"
+		;;
+esac
+
